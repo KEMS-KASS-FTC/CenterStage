d28a761f42334837fb8d331fab6feeb2375ed494
Ashfaq Chougle <ashfaqchougle@hotmail.com>
2023-11-30 21:07:17 -0600

    OpenCV Integration

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newAutonBlueRight.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+@Autonomous(name = "newAutonBlueRight")
+public class newAutonBlueRight extends LinearOpMode {
+    OpenCvCamera webcam;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        double distance = 0;
+        double correctionFactor = 1;
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        BlueRightPipe detector = new BlueRightPipe(telemetry);
+        webcam.setPipeline(detector);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                telemetry.addData("Error:","Camera cant stream");
+            }
+        });
+
+
+        waitForStart();
+
+
+
+
+    }
+}
